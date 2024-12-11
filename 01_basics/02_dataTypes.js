ab8bb2a0280325badc75311461a7d3715637acec
Aakanksha272001 <151499417+Aakanksha272001@users.noreply.github.com>
2024-12-11 03:27:32 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use Strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using node js not browser
+
+//DataTypes
+//primitive : 7
+//Number => 2 to power  53
+//bigInt
+//string  =>""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//Non primitive : 1
+//object
+
+console.log(typeof "Ankita");
+console.log(typeof 3);
+console.log(typeof null); //Importent
+console.log(typeof undefined);
