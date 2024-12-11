2e040f520e1623ea5ddbf27aae61ea4f469e8ef7
Aakanksha272001 <151499417+Aakanksha272001@users.noreply.github.com>
2024-12-11 03:54:23 +0000

    Conversion of data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+//NUMBER TO OTHER DATA TYPE
+
+// let score = 30
+
+// let StringScore = String(score);
+// let booleanScore = Boolean(score);
+// let objectScore = Object(score);
+// let symbolScore = Symbol(score);
+
+// console.log(typeof score);
+
+// console.log(typeof StringScore);
+// console.log( StringScore);
+// console.log(typeof booleanScore);
+// console.log( booleanScore);
+// console.log(typeof objectScore);
+// console.log( objectScore);
+// console.log(typeof symbolScore);
+// console.log( symbolScore);
+
+//STRING TO OTHER DATA TYPE
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let myname = "Ankita"
+
+let newname = Number(myname);
+console.log(typeof newname);
+console.log( newname);
+
+let booleanname = Boolean(myname);
+console.log(typeof booleanname);
+console.log( booleanname);
+
+let objectname = Object(myname);
+console.log(typeof objectname);
+console.log( objectname);
+
+// Boolean to other data type
+
+// 1 => true; 0=> false
+// " " => false
+// "hello" => true
