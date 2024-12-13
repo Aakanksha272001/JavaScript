0401425cafd138e883f7bbb002f3bba6f3f8f01e
Aakanksha Khanna <aakankshakhanna09@gmail.com>
2024-12-13 10:29:53 +0530

    String

---
+++ 01_basics/04_String.js
@@ -0,0 +1,28 @@
+//declear string 
+
+const name = "Aakanksha";
+const repoCount = 30;
+
+//OR
+
+const newname = new String("Ankita");
+
+console.log(newname);
+
+//console.log(`My name is ${name} and repo count is ${repoCount}`);  //String interpolation
+
+//String method
+
+// console.log(newname.__proto__);
+// console.log(newname.toUpperCase());
+// console.log(newname.charAt(2));
+// console.log(newname.indexOf('i'));
+// console.log(newname.substring(0,4));
+// console.log(newname.substring(-6,4)); //neative value not allow
+//console.log(newname.slice(-5,5));
+
+const value = "   hello    ";
+console.log(value);
+console.log(value.trim());
+
+//split(), include(),replace();
